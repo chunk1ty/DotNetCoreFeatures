fa83a7d0a0e5146cb7b4c1e229a35d149b7d4016
chunk1ty <andriyan.krastev@gmail.com>
2020-04-19 22:57:02 +0300

    add Configuration feature

---
+++ src/DotNetCoreFeatures.Configurations/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace DotNetCoreFeatures.Configurations.Controllers
+{
+    public class FeaturesConfiguration
+    {
+        public bool EnableAnkFeature { get; set; }
+
+        public string AnkText { get; set; }
+    }
+
+    public class HomeIndexViewModel
+    {
+        public HomeIndexViewModel(bool enableAnkFeature, string ankText)
+        {
+            EnableAnkFeature = enableAnkFeature;
+            AnkText = ankText;
+        }
+
+        public bool EnableAnkFeature { get; private set; }
+
+        public string AnkText { get; private set; }
+    }
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly FeaturesConfiguration _featuresConfiguration;
+
+        public HomeController(ILogger<HomeController> logger, 
+                              IConfiguration configuration, 
+                              IOptions<FeaturesConfiguration> featuresConfiguration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _featuresConfiguration = featuresConfiguration.Value;
+        }
+
+        public IActionResult Index()
+        {
+            // 1) directly 
+            //var enableAnkFeature =  _configuration.GetValue<bool>("Features:EnableAnkFeature");
+
+            // 2) section
+            var featuresSection = _configuration.GetSection("Features");
+            var enableAnkFeature = featuresSection.GetValue<bool>("EnableAnkFeature");
+
+            // 3) binding (names should match Features:EnableAnkFeature = feature.EnableAnkFeature)
+            //var feature = new FeaturesConfiguration();
+            //_configuration.Bind("Features", feature);
+
+            // 4) IOptions<FeaturesConfiguration> featuresConfiguration
+            // It should be registered  
+            // services.Configure<FeaturesConfiguration>(Configuration.GetSection("Features"));
+
+            return View(new HomeIndexViewModel(_featuresConfiguration.EnableAnkFeature, _featuresConfiguration.AnkText));
+        }
+    }
+}
---
+++ src/DotNetCoreFeatures.Configurations/Startup.cs
@@ -0,0 +1,39 @@
+using DotNetCoreFeatures.Configurations.Controllers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotNetCoreFeatures.Configurations
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.Configure<FeaturesConfiguration>(Configuration.GetSection("Features"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
